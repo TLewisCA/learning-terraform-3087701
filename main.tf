ce9bf6911bff1a6be6ce1540400acf05e8be7526
TLewisCA <136533956+TLewisCA@users.noreply.github.com>
2023-06-14 16:23:01 -0700

    fixed newline

--- main.tf
+++ main.tf
@@ -53,8 +53,8 @@ resource "aws_security_group_rule" "blog_https_in" {
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
-  
-}resource "aws_security_group_rule" "blog_http_in" {
+}  
+  resource "aws_security_group_rule" "blog_http_in" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
